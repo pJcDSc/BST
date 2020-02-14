778bf0190a6a5da75255a278fe04f14e483ea39a
U-BSD\411036 <411036@SHS-10DEC4F83.beaverton.k12.or.us>
2020-02-14 08:06:06 -0800

    Commented

--- BST.cpp
+++ BST.cpp
@@ -2,6 +2,7 @@
 Author: Peter Jin
 Date: 2/10/20
 Binary Search Tree Class
+this is the class that actually does everything
 */
 
 #include "BST.h"
@@ -15,7 +16,7 @@ BST::BST() {
   head = NULL;
 }
 
-//Public addNode function
+//Public addNode function which calls private addNode function
 void BST::addNode(int n){
   addNode(head, n);
   cout << "Node added" << endl;
@@ -23,71 +24,72 @@ void BST::addNode(int n){
 
 //private addnode function
 void BST::addNode(Node* &h, int n) {
-  if (h == NULL) {
-    head = new Node(n);
+  if (h == NULL) {          //If h is null then this was the head, and BST is empty
+    head = new Node(n); 
     return;
   }
-  else if (n < h -> getValue()) {
-    if (h -> getLeft() == NULL) {
+  else if (n < h -> getValue()) { //If value to add is less than current node, then we need to go to the left
+    if (h -> getLeft() == NULL) { //if there is nothing to the left then add it
       h -> setLeft(new Node(n));
       h -> getLeft() -> setParent(h);
       return;
     }
-    Node* left = h -> getLeft();
+    Node* left = h -> getLeft();  //Otherwise call addnode again on the left node
     addNode(left, n);
     return;
   }
-  else {
-    if (h -> getRight() == NULL) {
+  else {                          //Otherwise we need to go to the right
+    if (h -> getRight() == NULL) {//If there is nothing on the right we add the node
       h -> setRight(new Node(n));
       h -> getRight() -> setParent(h);
       return;
     }
-    Node* right = h -> getRight();
-    addNode(right, n);
+    Node* right = h -> getRight();//Otherwise call addnode again on the right side
+    addNode(right, n);            
     return;
   }
 }
 
 //public deletenode function
 void BST::deleteNode(int n) {
-  if(deleteNode(head, n)) cout << "Node deleted" << endl;
+  //Call private deletenode function, which returns true if it deletes and returns false otherwise
+  if(deleteNode(head, n)) cout << "Node deleted" << endl; 
 }
 
 //private deletenode function
 bool BST::deleteNode(Node* &h, int n) {
-  if (h == NULL) {
+  if (h == NULL) {               //We have not found the node, return false
     cout << "Empty Tree or Node not found" << endl;
     return false;
   }
   //Node to delete found
-  if (h -> getValue() == n) {
+  if (h -> getValue() == n) {    //We have found the node, now separate based on how many children
     if (h -> getLeft() == NULL && h -> getRight() == NULL) { //0 children
-      if (h -> getParent() == NULL) {
+      if (h -> getParent() == NULL) { //If this is the root node, delete it, and set head to null
 	Node* temp = h;
 	h = NULL;
 	delete temp;
 	return true;
       }
-      if (h == h -> getParent() -> getLeft()) h -> getParent() -> setLeft(NULL);
+      if (h == h -> getParent() -> getLeft()) h -> getParent() -> setLeft(NULL); //Otherwise reconfigure parent's child pointer
       else h -> getParent() -> setRight(NULL);
       delete h;
     }
     else if (h -> getLeft() == NULL) { //right child
-      if (h -> getParent() == NULL) {
+      if (h -> getParent() == NULL) {  //if root node, just set this to the right node, and reconfigure parent pointer
 	Node* temp = h;
 	h = h -> getRight();
 	h -> setParent(NULL);
 	delete temp;
 	return true;
       }
-      Node* right = h -> getRight();
+      Node* right = h -> getRight();     
       if (h == h -> getParent() -> getLeft()) h -> getParent() -> setLeft(right); //Point the parent's child pointer to new child
       else h -> getParent() -> setRight(right);
-      right -> setParent(h -> getParent());
+      right -> setParent(h -> getParent());                                       //Re set the parent pointer
       delete h;
     }
-    else if (h -> getRight() == NULL) { //left child
+    else if (h -> getRight() == NULL) { //left child                  //same thing as the right child
       if (h -> getParent() == NULL) {
 	Node* temp = h;
 	h = h -> getLeft();
@@ -102,31 +104,31 @@ bool BST::deleteNode(Node* &h, int n) {
       delete h;
     }
     else { //2 children
-      //Get inorder successor (only called on upper nodes, so don't need to deal with leaves)
-      Node* temp = h -> getRight();
+      //Get inorder successor (only called on upper nodes, so don't need to deal with inorder succ. for leaves)
+      Node* temp = h -> getRight(); //Inorder successor for not-leaves is one right then as many left as possible
       while (temp -> getLeft()) {
 	temp = temp -> getLeft();
       }
 
-      int value = temp -> getValue();
+      int value = temp -> getValue(); //Delete the inorder succ., then set the temps value to this
       deleteNode(temp, value);
       
       //Swap values as normal
-      h -> setValue(temp -> getValue());
+      h -> setValue(temp -> getValue()); //set the value of this node to inorder succ.
     }
-    return true;
+    return true; //We deleted it so return true
   }
 
-  if (n < h -> getValue()) {
-    if (h -> getLeft() == NULL) {
+  if (n < h -> getValue()) { //If value we are looking for is less than this value then go left
+    if (h -> getLeft() == NULL) { //If there is nothing to the left the value we are looking for doesn't exist
       cout << "Node not found" << endl;
       return false;
     }
     Node* t = h -> getLeft();
-    return deleteNode(t, n);
+    return deleteNode(t, n); //Call deletenode on left node
   }
-  else {
-    if (h -> getRight() == NULL) {
+  else {                     //If value we are looking for is more than this value go right
+    if (h -> getRight() == NULL) { //Same as left
       cout << "Node not found" << endl;
       return false;
     }
@@ -137,11 +139,11 @@ bool BST::deleteNode(Node* &h, int n) {
 
 //public display function
 void BST::displayTree() {
-  displayTree(head, 0);
+  displayTree(head, 0); //call private display function
 }
 
 //private display function
-void BST::displayTree(Node* h, int d) {
+void BST::displayTree(Node* h, int d) { //inorder print of bst
   if(!h) return; //empty tree
   if (h -> getLeft()) displayTree(h->getLeft(), d+1);
   for (int i = 0; i < d; i++) cout << "   ";
@@ -150,7 +152,7 @@ void BST::displayTree(Node* h, int d) {
 }
 
 //destructor
-BST::~BST() {
+BST::~BST() { //Repeatedly call delete on head until tree is gone
   while (head) { //Delete tree
     deleteNode(head, head -> getValue());
   }
--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 Author: Peter Jin
 Date: 2/11/20
 Binary Search Tree Main
+This actually just calls functions in BST class
 */
 
 #include "BST.h"
@@ -11,58 +12,60 @@ Binary Search Tree Main
 
 using namespace std;
 
-bool parse(char*, BST*);
-void printHelp();
-void addNode(BST*);
-void deleteNode(BST*);
-void display(BST*);
+bool parse(char*, BST*); //Parse input and call commands
+void printHelp();        //Prints the help 
+void addNode(BST*);      //Add a node to the BST
+void deleteNode(BST*);   //Delete a node from the BST (by number)
+void display(BST*);      //Display the BST
 
 int main() {
-  BST* bst = new BST();
-  bool running = true;
-  char* in = new char();
+  BST* bst = new BST();  //Actual BST that does everything
+  bool running = true;   //While loop var
+  char* in = new char(); //Input char
 
   cout << "Welcome to Binary Search Tree demo" << endl;
   cout << "Commands are ADD, REMOVE, DISPLAY, and QUIT. Type HELP for more info." << endl;
   
-  while (running) {
-    cin.get(in, 70);
+  while (running) {      //Until parse returns false ("QUIT") keep reading and calling parse
+    cin.get(in, 70);     
     cin.clear();
     cin.ignore(999, '\n');
 
-    running = parse(in, bst);
+    running = parse(in, bst); //Parse the input
   }
 
-  delete bst;
+  delete bst;            //Delete the BST when quit
 }
 
+//Parse input and call functions based on input
 bool parse(char* in, BST* bst) {
   for(int i = 0; i < strlen(in); i++) {
     in[i] = toupper(in[i]); //Capitalize input
   }
 
   if (strcmp(in, "HELP") == 0) {
-    printHelp();
+    printHelp();                             //Call help on HELP
   }
   else if (strcmp(in, "ADD") == 0) {
-    addNode(bst);
+    addNode(bst);                            //Call addnode on ADD
   }
-  else if (strcmp(in, "REMOVE") == 0 || strcmp(in, "DEL") == 0 || strcmp(in, "DELETE") == 0) {
-    deleteNode(bst);
+  else if (strcmp(in, "REMOVE") == 0 || strcmp(in, "DEL") == 0 || strcmp(in, "RM") == 0) {
+    deleteNode(bst);                         //Call delete on REMOVE / RM / DEL
   }
   else if (strcmp(in, "DISPLAY") == 0 || strcmp(in, "DISP") == 0) {
-    display(bst);
+    display(bst);                            //call display on DISPLAY / DISP
   }
   else if (strcmp(in, "QUIT") == 0) {
-    return false;
+    return false;                            //Stop the while loop by returning false on QUIT
   }
   else {
-    cout << "Sorry, I don't recognize that command." << endl;
+    cout << "Sorry, I don't recognize that command." << endl; //Command is not recognized otherwise
   }
-  return true;
+  return true;                                //Return true, not QUITTING
 }
 
-void printHelp() {
+//Print help stuff
+void printHelp() {      
   cout << "Binary Search Tree command Help" << endl;
   cout << "ADD: add a node to the binary search tree" << endl;
   cout << "REMOVE: remove a node from the binary search tree" << endl;
@@ -70,6 +73,7 @@ void printHelp() {
   cout << "QUIT: exit the program" << endl;
 }
 
+//Call addnode in BST class
 void addNode(BST* bst) {
   cout << "What number to add?" << endl;
   int n;
@@ -79,6 +83,7 @@ void addNode(BST* bst) {
   bst -> addNode(n);
 }
 
+//Call deletenode in BST class
 void deleteNode(BST* bst) {
   cout << "What number to delete?" << endl;
   int n;
@@ -88,7 +93,7 @@ void deleteNode(BST* bst) {
   bst -> deleteNode(n);
 }
 
+//call displayTree in BST class
 void display(BST* bst) {
   bst -> displayTree();
-}
-  
+}  
